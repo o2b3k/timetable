f4ad0bf7df1c32187a6084fea154efce528ff8df
o2b3k <im.o2b3k@gmail.com>
2017-04-29 11:33:42 +0600

    changed model Message

--- src/main/java/kg/ksucta/domain/model/Message.java
+++ src/main/java/kg/ksucta/domain/model/Message.java
@@ -1,9 +1,6 @@
 package kg.ksucta.domain.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by Bahriddin on 27.04.2017.
@@ -14,5 +11,59 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    
+    @Column(name = "subject_id")
+    private Integer subject_id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "created_at")
+    private String created_at;
+
+    @Column(name = "user_id")
+    private Integer user_id;
+
+    public Message(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(Integer subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
 }
