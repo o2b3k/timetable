82fd81588e39f283bb422a5a4d59bc040c6c61a8
talgatm <talgatm07@gmail.com>
2017-04-29 06:08:46 +0600

    Enum

---
+++ src/main/java/kg/ksucta/domain/model/Day_of_week.java
@@ -0,0 +1,14 @@
+package kg.ksucta.domain.model;
+
+/**
+ * Created by MT on 29.04.2017.
+ */
+public enum Day_of_week {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday;
+}
